88880ea43e29a88021c9437a2f7f4ec01a2a7175
Demontie costa <demontiecosta17@gmail.com>
2018-08-05 00:51:47 -0300

    correcao da conexão do mongodb

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ var express = require('express'),
 
 // conexão com o mongodb
 //mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://dev:dev112233@ds113122.mlab.com:13122/pessoas');
+//Pode mudar, fazendo a instalação do mongodb(https://www.mongodb.com/) mas essa url esta sendo utilizada no mlab(https://mlab.com), essa url ficará invalida!
+//Tem que mudar essa url 
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds113122.mlab.com:13122/pessoas');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+//Para o node aceitar requisições
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
